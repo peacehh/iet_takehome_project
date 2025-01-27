5a4e36b7cbc72c2592ccbb30566c7b9768c1463c
peacehh <findhamza.h@gmail.com>
2025-01-27 13:59:11 -0800

    load persons.csv as json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
-
+import fs from 'fs';
+import Papa from 'papaparse';
 const app = express();
 const PORT = 8080;
 
@@ -8,3 +9,19 @@ app.listen(
     PORT,
     () => console.log(`its alive on http://localhost:${PORT}`)
 );
+
+
+//load persons.csv in memory
+const load_persons = async () => {
+    try {
+        const data = await fs.promises.readFile('src/persons.csv', 'utf8');
+        const persons = Papa.parse(data, { header: true }).data;
+        return persons;
+    } catch (err) {
+        console.error('Error parsing person data:', err);
+        throw err;
+    }
+};
+
+let persons_json = await load_persons();
+
