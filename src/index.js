d55182229bf94b06d4e2ec42ecbb38168cb05f19
peacehh <findhamza.h@gmail.com>
2025-01-27 13:24:36 -0800

    change to es module

--- src/index.js
+++ src/index.js
@@ -1,27 +1,10 @@
-const app = require('express')();
+import express from 'express';
+
+const app = express();
 const PORT = 8080;
+
 
 app.listen(
     PORT,
     () => console.log(`its alive on http://localhost:${PORT}`)
-)
-
-app.get('/test', (req, res) => {
-    res.status(200).send({
-        tshirts: 15,
-        size: 'large'
-    })
-});
-
-app.post('/tshirts/:id', (req, res) => {
-    const {id} = req.params;
-    const {logo} = req.body;
-
-    if (!logo) {
-        res.status(418).send({message: 'We need a logo!'})
-    }
-
-    res.send({
-        tshirt: `shirt with your logo ${logo}`
-    })
-})
+);
