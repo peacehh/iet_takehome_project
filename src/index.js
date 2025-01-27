4715a6bf9d6ae5d9f0bca380a12443dcc68a5eb6
peacehh <findhamza.h@gmail.com>
2025-01-27 14:34:24 -0800

    validate person data

--- src/index.js
+++ src/index.js
@@ -16,6 +16,9 @@ const load_persons = async () => {
     try {
         const data = await fs.promises.readFile('src/persons.csv', 'utf8');
         const persons = Papa.parse(data, { header: true }).data;
+        persons.forEach(person => {
+            person.Salary = parseFloat(person.Salary);
+        });
         return persons;
     } catch (err) {
         console.error('Error parsing person data:', err);
@@ -24,4 +27,19 @@ const load_persons = async () => {
 };
 
 let persons_json = await load_persons();
+console.log(persons_json)
+
+//validate persons data
+let invalid_persons = [];
+persons_json.forEach(person => {
+    if (typeof person.First_Name !== 'string' ||
+        typeof person.Last_Name !== 'string' ||
+        typeof person.Email !== 'string' ||
+        !/^\S+@\S+\.\S+$/.test(person.Email.toLowerCase()) ||
+        typeof person.Salary !== 'number' ||
+        person.Salary < 0) 
+    {
+        invalid_persons.push(person.Id);
+    }
+});
 
