0f3755c5700fbf465ce404d9ef29ef63cfd1dba5
peacehh <findhamza.h@gmail.com>
2025-01-27 16:42:14 -0800

    add routing for /persons

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Papa from 'papaparse';
 //load and validate persons.csv in memory
 const load_persons = async () => {
     const data = await fs.promises.readFile('src/persons.csv', 'utf8');
-    const persons = Papa.parse(data, { header: true }).data;
+    let persons = Papa.parse(data, { header: true }).data;
     persons.forEach(person => {
         person.Salary = Number(person.Salary);
     });
@@ -15,11 +15,13 @@ const load_persons = async () => {
         throw "Dulplicate Person ID"
     };
     
-    persons.forEach(person => {
+    //skip any person with invalid data. display which person has invalid data.
+    persons = persons.filter(person => {
         if (!validate_person(person)) {
-            throw `Person validation failed for ID: ${person.Id} `
+            console.log(`Person validation failed for ID: ${person.Id}, skipping person`);
         }
-    });
+        return validate_person(person);
+    });    
 
     return persons;
 };
@@ -27,7 +29,6 @@ const load_persons = async () => {
 const validate_unique_Id = (persons) => {
     const persons_Ids = persons.map(person => person.Id);
     const persons_Ids_set = new Set(persons_Ids);
-
     return persons_Ids.length === persons_Ids_set.size;
 }
 
@@ -41,12 +42,20 @@ const validate_person = (person) => (
     person.Salary > 0
 );
 
-
-app.listen(
-    PORT,
-    () => console.log(`its alive on http://localhost:${PORT}`)
-);
-
 let persons_json = await load_persons();
 console.log(persons_json)
+
+const app = express();
+const PORT = 8080;
+
+
+//start server
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+app.get('/persons', (req, res) => {
+    res.send(persons_json.slice(0, 5));
+});
+
 
